7465212440b41fb481ee27c06842270c36a1de45
CoralieSanorana <coralie.rakotomalala@gmail.com>
2025-06-30 14:27:10 +0300

    tohiny 3

--- tp22/pages/fiche.php
+++ tp22/pages/fiche.php
@@ -15,27 +15,29 @@
         <div class="fiche text-center">
             <p>Nom :<?= $employer['last_name']; ?></p>
             <p>Prenom : <?= $employer['first_name']; ?></p>
-            <p>Date de naissance : <?= $employer['birth_date']; ?></p>
             <p>Genre : <?= $employer['gender']; ?></p>
+            <p>Date de naissance : <?= $employer['birth_date']; ?></p>
             <p>Date d'emboche : <?= $employer['hire_date']; ?></p>
-            <p>Historique des salaires :</p>
             <?php if($salaires != null){ ?> 
+                <p><h2>Historique des salaires :</h2></p>
                 <table border="1">
                     <tr>
                         <th>Date debut</th>
-                        <th>Date fin</th>
                         <th>Montant du salaire</th>
+                        <th>Date fin</th>
                     </tr>
                     <?php foreach($salaires as $salaire){ ?> 
                         <tr>
                             <td><?= $salaire['from_date']; ?></td>
-                            <td><?= $salaire['to_date']; ?></td>
                             <td><?= $salaire['salary']; ?></td>
+                            <td><?= $salaire['to_date']; ?></td>
                         </tr>
                     <?php } ?>
                 </table>
             <?php } ?>
+            
             <?php if($titres != null){ ?> 
+                <p><h2>Historique d'employe :</h2></p>
                 <table border="1">
                     <tr>
                         <th>Titre</th>
