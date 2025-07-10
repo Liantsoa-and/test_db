73f1d972a494b034b4271b31df5ee42cef356aeb
Liantsoa <liantsoaandreane@gmail.com>
2025-07-10 10:13:24 +0300

    modele-1

--- tp22/pages/modele.php
+++ tp22/pages/modele.php
@@ -18,10 +18,13 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="modele.php?pnum=index.php"><strong> Departements</strong></a>
+          <a class="nav-link active" aria-current="page" href="modele.php?pnum=index.php"><strong>Accueil</strong></a>
         </li>
         <li class="nav-item">
           <a class="nav-link active" aria-current="page" href="modele.php?pnum=liste.php"><strong> Employees par genre</strong></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="modele.php?pnum=liste.php"><strong>Liste des employés</strong></a>
         </li>
       </ul>
       <form class="d-flex" role="search" action="formulaire.php?" >
@@ -47,7 +50,7 @@
         include("index.php");
     }
     ?>
-    
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9Gkcappw/JbQhG/JmFb5A" crossorigin="anonymous"></script>
 
 </body>
