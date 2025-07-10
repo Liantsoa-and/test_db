c23848abb22b5991762ea87f182d6284a9582d80
Liantsoa <liantsoaandreane@gmail.com>
2025-07-10 09:38:19 +0300

    premier changement css qui marche

--- tp22/pages/index.php
+++ tp22/pages/index.php
@@ -1,31 +1,44 @@
-<?php 
- $departements = manager_en_cours();
+<?php
+$departements = manager_en_cours();
 ?>
-    <header>
-        <h1>Liste de tous les departements :</h1>
+    <header class="mb-4 text-center">
+        <h1 class="display-4 text-primary">Découvrez nos Départements</h1>
+        <p class="lead">Plongez dans le cœur de notre organisation et explorez chaque département, son manager dévoué et le nombre d'employés qui y contribuent.</p>
     </header>
-    <main>
-        <table border="1">
-            <tr>
-                <th>Departement</th>
-                <th>Nom du manageur en cours</th>
-                <th>Nombre d'employees</th>
-            </tr>
-            <?php foreach($departements as $departement){ 
-                $isa = compter_emp($departement['dept_no']); ?> 
-                <tr>
-                    <td>
-                        <a href="modele.php?id_dep=<?= $departement['dept_no']; ?>&page=1&pnum=employer.php">
-                            <p><?= $departement['dept_name']; ?></p>
-                        </a>
-                    </td>
-                    <td>
-                            <p><?= $departement['first_name']; ?> <?= $departement['last_name']; ?></p>
-                    </td>
-                    <td>
-                        <?= $isa['isa']; ?>
-                    </td>
-                </tr>
-            <?php } ?>
-        </table>
-    </main>
+
+        <main>
+            <div class="table-responsive">
+                <table class="table table-hover table-striped shadow-sm">
+                    <thead class="table-dark">
+                        <tr>
+                            <th scope="col">Département</th>
+                            <th scope="col">Manager Actuel</th>
+                            <th scope="col">Nombre d'employés</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php foreach($departements as $departement){
+                            $isa = compter_emp($departement['dept_no']); ?>
+                            <tr>
+                                <td>
+                                    <a href="modele.php?id_dep=<?= $departement['dept_no']; ?>&page=1&pnum=employer.php" class="text-decoration-none fw-bold text-info"> <?= htmlspecialchars($departement['dept_name']); ?>
+                                    </a>
+                                </td>
+                                <td>
+                                    <span class="text-secondary"><?= htmlspecialchars($departement['first_name'] . ' ' . $departement['last_name']); ?></span>
+                                </td>
+                                <td>
+                                    <span class="badge bg-success rounded-pill"><?= htmlspecialchars($isa['isa']); ?></span> </td>
+                            </tr>
+                        <?php } ?>
+                        <?php if (empty($departements)): ?>
+                            <tr>
+                                <td colspan="3" class="text-center py-4">Aucun département à afficher pour le moment. Revenez bientôt !</td>
+                            </tr>
+                        <?php endif; ?>
+                    </tbody>
+                </table>
+            </div>
+        </main>
+    </div>
+
--- tp22/pages/modele.php
+++ tp22/pages/modele.php
@@ -47,5 +47,8 @@
         include("index.php");
     }
     ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9Gkcappw/JbQhG/JmFb5A" crossorigin="anonymous"></script>
+
 </body>
 </html>
